7fb3a09b07adade41146a78e242401aa21d08852
sonniccc <sonnic317@gmail.com>
2025-09-13 17:13:04 -0400

    Styled button

--- src/components/AnimalIcon.js
+++ src/components/AnimalIcon.js
@@ -5,7 +5,6 @@ const AnimalIcon = ({ animal, clickCount, onClick }) => {
   return (
     <div className="animal-icon" onClick={onClick}>
       <div className="animal-emoji">{animal.emoji}</div>
-      <div className="animal-name">{animal.name}</div>
       <div className="click-count">{clickCount} clicks</div>
     </div>
   );
